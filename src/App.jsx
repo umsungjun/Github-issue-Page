982850b4df8ea4f180f9ac962c9071873ebff285
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-03-24 00:58:39 +0900

    헤더 버튼 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,64 @@
 import styles from "./App.module.css"
 
+import { SlEye } from "react-icons/sl"
+import { TbGitFork } from "react-icons/tb"
+import { AiOutlineStar } from "react-icons/ai"
+
 function App() {
   return (
     <>
       <div className={styles.nav}>Nav</div>
-      <div className={styles.header}>Header</div>
-      <div className={styles.listContainer}>ListContainer</div>
+      <div className={styles.header}>
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+          }}
+        >
+          <SlEye />
+          Watch
+        </Button>
+        <Space />
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+          }}
+        >
+          <TbGitFork /> Fork <div className={styles.circle}>5</div>
+        </Button>
+        <Space />
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+          }}
+        >
+          <AiOutlineStar /> Star
+        </Button>
+      </div>
+      <div className={styles.listContainer}>
+        <Button
+          style={{ fontSize: "14px", backgroundColor: "green", color: "#fff" }}
+        >
+          New Issue
+        </Button>
+      </div>
       <div className={styles.footer}></div>
     </>
   )
+}
+
+function Button({ style, children }) {
+  return (
+    <button className={styles.button} style={style}>
+      {children}
+    </button>
+  )
+}
+
+function Space() {
+  return <div className={styles.space}></div>
 }
 
 export default App
